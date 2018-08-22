6d1eb339838c10781647484a1cf5bf6fafa83576
ClaireBvs <clairembeauvais@gmail.com>
2018-08-22 15:39:41 -0600

    add method to call api and display info in view / hash

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'nokogiri'
   gem 'rspec-rails'
   gem 'database_cleaner'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
 
   def show
     conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["Authorization"] = current_user.oauth_token
+      faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
       faraday.adapter Faraday.default_adapter
     end
 
-    response = conn.get("/api/#{current_user.nickname}")
+    response = conn.get("/users/#{current_user.nickname}")
 
-    @followers = JSON.parse(response.body, symbolize_names: true)[:results]
+    @info = JSON.parse(response.body, symbolize_names: true)
   end
 end
