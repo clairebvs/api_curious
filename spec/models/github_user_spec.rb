60dc491261c7031a32fca5afa90608fd1ea598ad
ClaireBvs <clairembeauvais@gmail.com>
2018-08-23 15:22:54 -0600

    Add test for instance method repositories

--- spec/models/github_user_spec.rb
+++ spec/models/github_user_spec.rb
@@ -11,4 +11,29 @@ describe GithubUser do
     expect(github_user).to be_a(GithubUser)
   end
 
+  context "instance methods" do
+    context "#repositories" do
+      it 'returns repositories for a user' do
+        user = create(:user)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        data = {
+                id: 141,
+                node_id: "MDE",
+                name: "1804"
+                }
+        data2 = {
+                id: 14,
+                node_id: "E",
+                name: "Mast"
+                }
+
+        github_user = GithubUser.new(user)
+        repo1 = Repository.new(data)
+        repo2 = Repository.new(data2)
+
+        expect(github_user.repositories.count).to eq(2)
+        expect(github_user.repositories.first).to be_a(Repository)
+      end
+    end
+  end
 end
