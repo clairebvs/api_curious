e6c398722809e20667b03dd8140302f94b14d83d
ClaireBvs <clairembeauvais@gmail.com>
2018-08-22 21:06:48 -0600

    add github service for api call

---
+++ app/services/github_service.rb
@@ -0,0 +1,24 @@
+class GithubService
+
+  def initialize(current_user)
+    @current_user = current_user
+  end
+
+  def user_info
+    get_json("/users/#{current_user.nickname}")
+  end
+
+  private
+
+  def conn
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+
+end
