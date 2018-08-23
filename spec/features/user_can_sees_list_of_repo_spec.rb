f9a42318d04c8502ac7913c08079d91e45cc356e
ClaireBvs <clairembeauvais@gmail.com>
2018-08-23 13:06:45 -0600

    Add feature test user sees list repo

---
+++ spec/features/user_can_sees_list_of_repo_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'user logged in' do
+  scenario 'can see list of their repositories' do
+    user = create(:user)
+
+    # allow_any_instance_of(User).to receive(:current_user).and_return(user)
+    visit '/repositories'
+
+    expect(page).to have_css('repository')
+    expect(page).to have_content("List of repositories :")
+
+  end
+end
