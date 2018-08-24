5d15833fb51d491cf72484c9c854d66f68d01ee9
ClaireBvs <clairembeauvais@gmail.com>
2018-08-23 20:34:43 -0600

    add refactor githubservice method for github user

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,15 +3,15 @@ class RepositoriesController < ApplicationController
   def index
     @repositories = GithubUser.new(current_user).repositories
 
-    conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = conn.get("/users/#{current_user.nickname}/repos")
-
-    @repositories = JSON.parse(response.body, symbolize_names: true).map do |repo|
-      Repository.new(repo)
-    end
+    # conn = Faraday.new(url: "https://api.github.com") do |faraday|
+    #   faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("/users/#{current_user.nickname}/repos")
+    #
+    # @repositories = JSON.parse(response.body, symbolize_names: true).map do |repo|
+    #   Repository.new(repo)
+    # end
   end
 end
