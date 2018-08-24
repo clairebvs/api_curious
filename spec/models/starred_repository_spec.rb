9a1a5fd39db8c84df30ecbaeb8b89eac99df0eaa
ClaireBvs <clairembeauvais@gmail.com>
2018-08-24 09:36:45 -0600

    add model starred_repository

--- spec/models/starred_repository_spec.rb
+++ spec/models/starred_repository_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 describe StarredRepository do
   it 'find starred repositories data' do
-    user = create(:user)
+    data = {
+            id: 12,
+            node_id: "MDE",
+            name: "idea-box"
+            }
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    starred_repository = StarredRepository.new(data)
 
-    json_response = File.open("./fixtures/starred_repos.json")
-
-    WebMock.stub_request(:get, "https://api.github.com//users/user/starred").to_return(status: 200, body: json_response)
-
-    starred_repository = StarredRepository.new(json_response)
-
-    expect(repository.name).to eq('idea_box')
+    expect(starred_repository.id).to eq(12)
+    expect(starred_repository.name).to eq('idea-box')
   end
 end
