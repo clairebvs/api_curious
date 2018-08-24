de71e97d8416b6ab74386e0cfadcb76274847a5b
ClaireBvs <clairembeauvais@gmail.com>
2018-08-24 09:19:59 -0600

    add feature test user sees starred repos and gem webmock

---
+++ spec/features/user_can_see_list_of_starred_repos_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'user logged in' do
+  scenario 'can see list of their starred repos' do
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    json_response = File.open("./fixtures/starred_repos.json")
+
+    WebMock.stub_request(:get, "https://api.github.com//users/user/starred").to_return(status: 200, body: json_response)
+
+    expect(page).to have_css('.starred_repo', count: 2)
+    expect(page).to have_content("List of starred repositories :")
+
+    within(first('.starred_repo')) do
+      expect(page).to have_content('idea-box')
+    end
+  end
+end
