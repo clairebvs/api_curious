2f7c4d33f65bc6a42d0aa3e99d3631e8d86a45c0
ClaireBvs <clairembeauvais@gmail.com>
2018-08-24 09:47:50 -0600

    add service test for user_starred_repositories method

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -30,5 +30,21 @@ describe GithubService do
         # end
       end
     end
+    context "#user_starred_repositories" do
+      it 'returns starred repositories for a user from api call' do
+        # VCR.use_cassette("github_service.user_repositories") do
+          user = create(:user)
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+          github_service = GithubService.new(user)
+          # require "pry"; binding.pry
+
+          repo = github_service.user_starred_repositories.first
+          expect(github_service.user_starred_repositories.count).to eq(2)
+          expect(repo).to have_key(:name)
+          expect(repo).to have_key(:id)
+        # end
+      end
+    end
   end
 end
