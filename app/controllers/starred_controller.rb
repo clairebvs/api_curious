1d6e821b89ebda27240fdad4ee58a9397da90e82
ClaireBvs <clairembeauvais@gmail.com>
2018-08-24 09:27:54 -0600

    add method to call api for starred repo

--- app/controllers/starred_controller.rb
+++ app/controllers/starred_controller.rb
@@ -1,6 +1,16 @@
 class StarredController < ApplicationController
 
   def index
+    conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/users/#{current_user.nickname}/starred")
+
+    @starred_repositories = JSON.parse(response.body, symbolize_names: true).map do |repo|
+      StarredRepository.new(repo)
+    end 
   end
 
 end
