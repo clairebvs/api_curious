abc790af653a7923bb8ba2a3f660d8559b1b5091
ClaireBvs <clairembeauvais@gmail.com>
2018-08-26 13:13:55 -0600

    add css to center tile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @github_user = FollowerPresenter.new(current_user)
-    @starred_repositories = GithubUser.new(current_user).starred_repositories
 
     # conn = Faraday.new(url: "https://api.github.com") do |faraday|
     #   faraday.headers["Authorization"] = "token #{current_user.oauth_token}"
