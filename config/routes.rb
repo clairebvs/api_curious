a1491d69a0f041466598992dc4f555610cc8b7a9
ClaireBvs <clairembeauvais@gmail.com>
2018-08-25 23:17:03 -0600

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: "users#show"
   get '/repository', to: "repositories#index"
-  # get '/starred', to: "starred#index"
-  get '/dashboard', to: "starred#index"
+  get '/starred', to: "starred#index"
 end
